c2705a5d00f61cac3a408fc7e4012a99c9e111aa
fsvieira <sv.filipe@gmail.com>
2014-10-15 17:57:40 +0100

    fix pub

--- balance/platforms/android/ant-build/Balance.ap_.d
+++ balance/platforms/android/ant-build/Balance.ap_.d
@@ -6,13 +6,10 @@
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-port-hdpi/screen.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-land-hdpi/screen.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/xml/config.xml \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-ldpi/icon.png \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-xhdpi/icon.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-xhdpi/ic_action_remove.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-xhdpi/ic_action_previous_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-xhdpi/ic_action_next_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-port-ldpi/screen.png \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-hdpi/icon.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-hdpi/ic_action_remove.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-hdpi/ic_action_previous_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/res/drawable-hdpi/ic_action_next_item.png \
@@ -33,13 +30,10 @@
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/xml/config.xml \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-ldpi/icon.png \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-xhdpi/ic_action_remove.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-xhdpi/ic_action_previous_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-xhdpi/ic_action_next_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
-/home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-hdpi/ic_action_remove.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-hdpi/ic_action_previous_item.png \
 /home/fsvieira/data/projects/apps/balance-git/balance/platforms/android/ant-build/res/drawable-hdpi/ic_action_next_item.png \
